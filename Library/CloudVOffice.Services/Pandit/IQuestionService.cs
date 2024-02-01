918f968b02dc01f23e514f5ed510fda00db7874c
Trinatha kumar sethy <trinathsethy872@gmail.com>
2024-02-01 12:32:46 +0530

    Question Crud Operation

---
+++ Library/CloudVOffice.Services/Pandit/IQuestionService.cs
@@ -0,0 +1,20 @@
+﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.Pandit;
+using CloudVOffice.Data.DTO.Pandit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.Pandit
+{
+    public interface IQuestionService
+    {
+        public MessageEnum QuestionCreate(QuestionDTO questionDTO);
+        public MessageEnum QuestionUpdate(QuestionDTO questionDTO);
+        public MessageEnum DeleteQuestion(int QuestionId, Int64 DeletedBy);
+        public List<Question> GetQuestionList();
+        public Question GetQuestionById(int QuestionId);
+    }
+}
